d03796f8d0c4e1e7b065103f8b4ac66718f43cc0
RashaM <rashamassoud1998@gmail.com>
2023-03-05 22:52:04 +0200

    Created a GET API that takes a string number and sort FE

---
+++ selectionSort.js
@@ -0,0 +1,19 @@
+let send_btn = document.getElementById("send");
+send_btn.addEventListener("click", send);
+
+function send() {
+    let strOfNumbers=document.getElementById("strOfNumbers").value;
+    let data = new FormData();
+
+    data.append("strOfNumbers",strOfNumbers);
+
+    axios({
+        "method":"get",
+        "url":"http://localhost/APIsUsingPHP/selectionSort.php",
+        "data":data
+    }).then((res)=>{
+        console.log(res);
+    }).catch((err)=>{
+        console.log(err);
+    });
+}
