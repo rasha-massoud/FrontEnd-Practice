15e1a9af7a4608912415b0fa5664c323886f3eeb
RashaM <rashamassoud1998@gmail.com>
2023-03-06 00:14:54 +0200

    Modified the selection sort FE file

--- selectionSort.js
+++ selectionSort.js
@@ -2,18 +2,17 @@ let send_btn = document.getElementById("send");
 send_btn.addEventListener("click", send);
 
 function send() {
-    let strOfNumbers=document.getElementById("strOfNumbers").value;
+    let strOfNumbers = document.getElementById("strOfNumbers").value;
     let data = new FormData();
-
-    data.append("strOfNumbers",strOfNumbers);
+    data.append("strOfNumbers", strOfNumbers);
 
     axios({
-        "method":"get",
-        "url":"http://localhost/APIsUsingPHP/selectionSort.php",
-        "data":data
-    }).then((res)=>{
+        "method": "get",
+        "url": "http://localhost/APIsUsingPHP/selectionSort.php",
+        "data": data
+    }).then((res) => {
         console.log(res);
-    }).catch((err)=>{
+    }).catch((err) => {
         console.log(err);
     });
 }
